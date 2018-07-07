11fbc66b5dcaef43c5eb44685f015844f9890715
PratsJadhav <jadhav.pratiksha001@gmail.com>
2018-07-07 12:20:17 +0530

    second commit

--- MyAutomation/src/main/java/TestBase/TestBase.java
+++ MyAutomation/src/main/java/TestBase/TestBase.java
@@ -1,8 +1,67 @@
 package TestBase;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class TestBase {
 
 	public static WebDriver driver;
+	public static Properties Config = null;
+	public File file;
+	public FileInputStream fis;
+    public static Logger log = null;
+	
+	public void init() throws IOException
+	{
+		log = Logger.getLogger("devpinoyLogger");
+		loadPropertiesFile();
+		selectBrowser(Config.getProperty("browser"));
+		getURL(Config.getProperty("url"));
+		
+	}
+
+
+	public void loadPropertiesFile() throws IOException 
+	{
+	  Config = new Properties();
+	  fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\Config\\Config.properties");
+	  Config.load(fis);
+	  log.debug("properties file loaded successfully...");
+	}
+	
+	public void selectBrowser(String browser)
+	{
+		if(browser.equalsIgnoreCase("Chrome"))
+		{
+			log.debug("opening chrome browser...");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("Firefox"))
+		{
+			log.debug("opening firefox browser...");
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+	}
+	
+    public void getURL(String url) 
+    {
+		driver.get(url);
+		log.debug("url opened - "+url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 }
